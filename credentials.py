2a0bedbfc12af44d7520e8bc45baf45196242a26
4xk <41323499+4xk@users.noreply.github.com>
2018-09-19 23:15:29 -0400

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,7 @@
+PASS = "your.token" # OAUTH Token
+NICK = "viperbotv2"            # Bot username
+CHAN = "miestasbubba"     # Channel to join
+
+PASS = PASS.lower()    
+NICK = NICK.lower()  
+CHAN = CHAN.lower()
